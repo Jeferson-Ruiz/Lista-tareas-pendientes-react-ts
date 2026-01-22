cffa2d8da7cd903b27a58eda23bb0d508aaf8662
Jeferson-Ruiz <jefersonruiz587@gmail.com>
2026-01-22 11:11:35 -0500

    proyecto typescripth

---
+++ src/components/Tarea.tsx
@@ -0,0 +1,14 @@
+type Props = {
+    tarea : string,
+    borrarTarea : () => void;
+}
+
+export const Tarea = ({tarea, borrarTarea}: Props) => {
+  return (
+    <div className="task">
+        <span>{tarea}</span>
+        <button onClick={borrarTarea}>Eliminar</button>
+
+    </div>
+  )
+}
